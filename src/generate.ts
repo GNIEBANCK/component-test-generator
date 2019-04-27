0ddbaaa5387c5226cda2c6b669225a7273c54df2
Gregory Niebanck <gniebanc@vt.edu>
2019-04-27 16:07:56 -0400

    testing

--- src/generate.ts
+++ src/generate.ts
@@ -1,6 +1,13 @@
 import {generateComponentTestFiles} from './utils/generateComponentTestFiles'
+import JsonIO from './utils/JsonIO';
+
 async function generate()
 {
-    await generateComponentTestFiles();}
-
+    let io = new JsonIO();
+    let scenarios = (await io.readPlainText('../scenarios.txt')).split('\n');
+    let testCases = (await io.readPlainText('../testCases.txt')).split('\n');
+    console.log(scenarios);
+    console.log(testCases)
+    //await generateComponentTestFiles(scenarios,testCases);
+}
 generate().then(()=>console.log("done")).catch((e)=>console.log(e));
